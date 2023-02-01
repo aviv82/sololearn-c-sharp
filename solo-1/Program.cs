f51e89ec1d0b4273b4a83779009dfc3bcd5f5883
Aviv <aviv.dror@gmail.com>
2023-02-02 00:36:39 +0100

    ee

--- solo-1/Program.cs
+++ solo-1/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
 
+namespace Solo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# is cool");
+        }
+    }
+}
