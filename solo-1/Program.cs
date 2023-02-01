8630de9faa00f31f793b5fd81a20cc736ec2ceda
Aviv <aviv.dror@gmail.com>
2023-02-02 00:59:13 +0100

    lesson 5.1

--- solo-1/Program.cs
+++ solo-1/Program.cs
@@ -13,12 +13,32 @@ namespace Solo_1
     {
         static void Main(string[] args)
         {
+
+            //lesson 3.1
+
             Console.WriteLine("C# is cool");
+
+
+            // lesson 4.1
 
             int x = 3;
             int y = 4;
 
             Console.WriteLine("x={0}; y={1}", x,y);
+
+            //lesson 5.1
+
+            string name;
+            Console.WriteLine("what's your name?");
+            name = Console.ReadLine();
+
+            int age;
+            Console.WriteLine("how old are you?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("hi there, {0}! you are {1} years old.", name, age);
+
         }
     }
 }
