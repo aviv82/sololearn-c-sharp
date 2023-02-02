e0b319dd630326637aaaefb47b746d310fbea2e8
Aviv <aviv.dror@gmail.com>
2023-02-02 18:35:19 +0100

    lesson 13.1

--- solo-loop/Program.cs
+++ solo-loop/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// lesson 13.1
+
+Console.WriteLine("please input picture height");
+int height = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("please input picture width");
+int width = Convert.ToInt32(Console.ReadLine());
+
+if (height > width)
+{
+    Console.WriteLine("portrait"); }
+    else if (height == width)
+    {
+    Console.WriteLine("square");
+    }   
+    else
+    {
+    Console.WriteLine("landscape");
+    }
+
+Console.ReadLine();
