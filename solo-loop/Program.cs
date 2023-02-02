f427b13e04d22f4bc4914005ae217b522460adc8
Aviv <aviv.dror@gmail.com>
2023-02-02 18:56:16 +0100

    lesson 14.1

--- solo-loop/Program.cs
+++ solo-loop/Program.cs
@@ -21,4 +21,37 @@ if (height > width)
     Console.WriteLine("landscape");
     }
 
+// lesson 14.1
+
+Console.WriteLine("how many angles does your polygon have");
+int angles = Convert.ToInt32(Console.ReadLine());
+
+
+switch (angles)
+{
+    case 3:
+        Console.WriteLine("triangle");
+        break;
+    case 4:
+        Console.WriteLine("rectangle");
+        break;
+    case 5:
+        Console.WriteLine("pentagon");
+        break;
+    case 6:
+        Console.WriteLine("hectagon");
+        break;
+    case 7:
+        Console.WriteLine("septagon");
+        break;
+    case 8:
+        Console.WriteLine("hexagon");
+        break;
+    default:
+        Console.WriteLine("toomanyagon");
+        break;
+}
+
+
+
 Console.ReadLine();
