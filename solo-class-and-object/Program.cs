9ade007f17051be49a25c739f23b862c23598b8b
Aviv <aviv.dror@gmail.com>
2023-02-04 22:56:03 +0100

    lesson 37.1

--- solo-class-and-object/Program.cs
+++ solo-class-and-object/Program.cs
@@ -1,3 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+// lesson 37.1
+
+
+Bread bagguette = new Bread();
+
+
+bagguette.dough = "white";
+bagguette.name = "bagguette";
+bagguette.weight = 200;
+
+bagguette.Print();
+
+Bread brioche = new Bread();
+
+brioche.dough = "butter";
+brioche.name = "brioch";
+brioche.weight = 300;
+
+brioche.Print();
+
+class Bread
+{
+    public string dough;
+    public string name;
+    public int weight;
+
+    public void Print()
+    {
+        Console.WriteLine("this bread is called {0}, it is made with {1} dough and weighs {2} grams", name, dough, weight);
+    }
+}
+
 
